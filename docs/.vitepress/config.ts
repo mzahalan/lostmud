2f8c206e71d64375a2b1f256fbbf09b716c893c0
Mike Zahalan <mzahalan@amazon.com>
2024-05-08 22:21:09 -0700

    Trying to get the docs to work.

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "The Lost Manual",
   description: "The Missing Manual for Mud Development",
+  base: "/docs/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
