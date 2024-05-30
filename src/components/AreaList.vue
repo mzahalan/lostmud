3c9d3fa0a11377b29cbd4c7c2a84a640e1889109
Mike Zahalan <mzahalan@amazon.com>
2024-05-30 11:28:24 -0700

    Styled the Area Cards a bit.

--- src/components/AreaList.vue
+++ src/components/AreaList.vue
@@ -1,8 +1,9 @@
 <template>
     <v-container class="area-list">
-        <v-row>
-        <v-card v-for="area in areas" :key="area.name">
-            <span>{{area.name}}</span>
+        <v-row class="justify-center">
+        <v-card elevation="4" class="ma-2 text-capitalize" max-width="200" min-width="200" v-for="area in areas" :key="area.name">
+            <v-card-title>{{ area.name }}</v-card-title>
+            <v-card-subtitle>Rooms: {{ area.numRooms }}<v-spacer></v-spacer>Levels: {{ area.range }}</v-card-subtitle>
         </v-card>
 
         </v-row>
